ebe121c67911a1b937e44a4015b213a862521bf0
User <User@CQ.local>
2016-10-26 20:38:36 -0600

    home page is done

--- js/controller/homeCtrl.js
+++ js/controller/homeCtrl.js
@@ -27,35 +27,8 @@ angular.module('systemic').controller('homeCtrl', function($scope){
         }
       }
 
-      //console.log(InfiniteRotator);
-
-      var InfiniteRotatorBox = {
-        init: function(){
-          var initialFadeInBox = 2000;
-          var itemIntervalBox = 5000;
-          var fadeTimeBox = 4000;
-          var numberOfItemsBox = $('.box-move').length;
-          var currentItemBox = 0;
-
-          $('.box-move').eq(0).css('opacity', 0.2);
-
-          setInterval(function(){
-            console.log('box')
-            $('.box-move').eq(currentItemBox + 1).css('opacity', 0.2);
-            $('.box-move').eq(currentItemBox).css('opacity', 1);
-
-            if(currentItemBox === numberOfItemsBox - 1){
-              currentItemBox = 0;
-              $('.box-move').eq(0).css('opacity', 0.2);
-            } else {
-               currentItemBox++;
-            }
-          }, itemIntervalBox);
-        }
-      }
-
-      InfiniteRotatorBox.init();
       InfiniteRotator.init();
+
 
       $(function header (){
           var head = $('.navbar');
@@ -82,3 +55,34 @@ angular.module('systemic').controller('homeCtrl', function($scope){
       });
     })
 })
+
+//console.log(InfiniteRotator);
+
+// var InfiniteRotatorBox = {
+//   init: function(){
+//     var initialFadeInBox = 2000;
+//     var itemIntervalBox = 5000;
+//     var fadeTimeBox = 4000;
+//     var numberOfItemsBox = $('.box-move').length;
+//     var currentItemBox = 0;
+//
+//
+//     $('.box-move').eq(0).css('opacity', 0.2);
+//
+//     setInterval(function(){
+//       console.log('box')
+//       $('.box-move').eq(currentItemBox + 1).css('opacity', 0.2);
+//       $('.box-move').eq(currentItemBox).css('opacity', 1);
+//
+//       if(currentItemBox === numberOfItemsBox - 1 && currentItemBox !== numberOfItemsBox.length){
+//         currentItemBox = 0;
+//         console.log('this is home', currentItemBox)
+//         $('.box-move').eq(0).css('opacity', 0.2);
+//       } else {
+//          currentItemBox++;
+//       }
+//     }, itemIntervalBox);
+//   }
+// }
+//
+// InfiniteRotatorBox.init();
